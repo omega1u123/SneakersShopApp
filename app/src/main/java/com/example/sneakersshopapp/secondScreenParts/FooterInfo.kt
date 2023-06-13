772423417c7070c88df7d9b234999e6af18b227d
omega1u123 <gvardian2810@gmail.com>
2023-06-13 21:25:26 +0300

    add footer info

--- app/src/main/java/com/example/sneakersshopapp/secondScreenParts/FooterInfo.kt
+++ app/src/main/java/com/example/sneakersshopapp/secondScreenParts/FooterInfo.kt
@@ -1,12 +1,14 @@
 package com.example.sneakersshopapp.secondScreenParts
 
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
@@ -18,6 +20,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -47,14 +50,21 @@ fun FooterInfo(){
                         buttonColor3 = Color.Gray
                           },
                 colors = ButtonDefaults.buttonColors(Color.White),
-                contentPadding = PaddingValues(5.dp)
+                contentPadding = PaddingValues(10.dp)
 
             ) {
-                Text(
-                    text = "DESCRIPTION",
-                    color = buttonColor1,
-                    fontSize = 18.sp
-                )
+                Column() {
+                    Text(
+                        text = "DESCRIPTION",
+                        color = buttonColor1,
+                        fontSize = 18.sp
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    if (buttonColor1 == Color.Black ){
+                        Image(painter = painterResource(id = R.drawable.active_border), contentDescription = null, modifier = Modifier.width(50.dp))
+                    }
+                }
+
             }
             Spacer(modifier = Modifier.width(10.dp))
             Button(
@@ -64,14 +74,20 @@ fun FooterInfo(){
                     buttonColor1 = Color.Gray
                     buttonColor3 = Color.Gray  },
                 colors = ButtonDefaults.buttonColors(Color.White),
-                contentPadding = PaddingValues(5.dp)
+                contentPadding = PaddingValues(10.dp)
 
             ) {
-                Text(
-                    text = "DELIVERY",
-                    color = buttonColor2,
-                    fontSize = 18.sp
-                )
+                Column {
+                    Text(
+                        text = "DELIVERY",
+                        color = buttonColor2,
+                        fontSize = 18.sp
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    if (buttonColor2 == Color.Black ){
+                        Image(painter = painterResource(id = R.drawable.active_border), contentDescription = null, modifier = Modifier.width(50.dp))
+                    }
+                }
             }
             Spacer(modifier = Modifier.width(10.dp))
             Button(
@@ -80,17 +96,25 @@ fun FooterInfo(){
                     buttonColor1 = Color.Gray
                     buttonColor2 = Color.Gray },
                 colors = ButtonDefaults.buttonColors(Color.White),
-                contentPadding = PaddingValues(5.dp)
+                contentPadding = PaddingValues(10.dp)
 
             ) {
-                Text(
-                    text = "REVIEWS",
-                    color = buttonColor3,
-                    fontSize = 18.sp
-                )
+                Column {
+                    Text(
+                        text = "REVIEWS",
+                        color = buttonColor3,
+                        fontSize = 18.sp
+                    )
+                    Spacer(modifier = Modifier.height(5.dp))
+                    if (buttonColor3 == Color.Black ){
+                        Image(painter = painterResource(id = R.drawable.active_border), contentDescription = null, modifier = Modifier.width(50.dp))
+                    }
+                }
             }
         }
-        Spacer(modifier = Modifier.height(10.dp))
+
+        Spacer(modifier = Modifier.height(5.dp))
+
         if(buttonColor1 == Color.Black && buttonColor2 == Color.Gray && buttonColor3 == Color.Gray){
 
             Text(text = stringResource(id = R.string.button1))
