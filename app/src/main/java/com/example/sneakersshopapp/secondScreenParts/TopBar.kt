cd23aaf0d84fdfb243ae1076a7c23eb6edf8a1a9
omega1u123 <gvardian2810@gmail.com>
2023-06-25 20:05:41 +0300

    changed image to button

--- app/src/main/java/com/example/sneakersshopapp/secondScreenParts/TopBar.kt
+++ app/src/main/java/com/example/sneakersshopapp/secondScreenParts/TopBar.kt
@@ -2,14 +2,23 @@ package com.example.sneakersshopapp.secondScreenParts
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -23,12 +32,20 @@ fun TopBar(onClicked: () -> Unit){
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
         ) {
-        Image(
-            painter = painterResource(id = R.drawable.hamburger__1_),
-            contentDescription = null,
-            modifier = Modifier.height(25.dp)
-        )
-        Button(onClick = { onClicked() }){}
+
+        Button(
+            onClick = { onClicked() },
+            colors = ButtonDefaults.buttonColors(Color.White),
+            modifier = Modifier.width(50.dp).height(30.dp),
+            shape = RectangleShape,
+            contentPadding = PaddingValues(0.dp)
+        ){
+            Image(
+                painter = painterResource(id = R.drawable.hamburger__1_),
+                contentDescription = null,
+                modifier = Modifier.fillMaxWidth()
+            )
+        }
         Text(
             text = "Running",
             fontSize = 35.sp
